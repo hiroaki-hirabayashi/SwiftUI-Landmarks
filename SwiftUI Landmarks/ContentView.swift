7d12f22c3ce4b78c31eadee4a2fe3824d0b31274
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2020-12-17 00:25:11 +0900

    地図（MapView）とイメージ画像（CircleImage）を挿入してビュー画面を作成

--- SwiftUI Landmarks/ContentView.swift
+++ SwiftUI Landmarks/ContentView.swift
@@ -10,20 +10,34 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("HelloWorld!!!!!!!!")
-                .font(.title)
-            HStack {
-                Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
-                    .font(.subheadline)
-                Spacer()
-                
-                Text("California")
-                    .font(.subheadline)
+        VStack {
+            MapView() //作成した地図を表示と高さ、セーフエリアを超えて表示
+                .edgesIgnoringSafeArea(.top)
+                .frame(height: 300.0)
+          
+            CircleImage() //イメージ画像と位置
+                .offset(y: -130)
+                .padding(.bottom, -130.0)
+            
+            VStack(alignment: .leading) {
+                Text("HelloWorld!!!!!!!!")
+                    .font(.title)
+            
+                HStack {
+                    Text(/*@START_MENU_TOKEN@*/"Joshua Tree National Park"/*@END_MENU_TOKEN@*/)
+                        .font(.subheadline)
+                    Spacer()
+                    
+                    Text("California")
+                        .font(.subheadline)
+                }
+
             }
+            .padding()
+            
+            Spacer() //部品全体を上へ移動させる
 
         }
-        .padding()
     }
 }
 
--- SwiftUI Landmarks/MapView.swift
+++ SwiftUI Landmarks/MapView.swift
@@ -26,6 +26,7 @@ struct MapView: UIViewRepresentable {
     }
 }
 
+
 struct MapView_Previews: PreviewProvider {
     static var previews: some View {
         MapView()
