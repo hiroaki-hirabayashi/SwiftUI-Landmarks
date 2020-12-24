be9690eed8526967a6e0b367ffa34e4b7633e9b9
Hiroaki Hirabayashi <hh.38063806@gmail.com>
2020-12-24 15:17:18 +0900

    プロフィール画面右上に編集画面へのリンクボタン作成

--- SwiftUI Landmarks/Views/Profiles/ProfileHost.swift
+++ SwiftUI Landmarks/Views/Profiles/ProfileHost.swift
@@ -7,14 +7,19 @@
 //
 
 import SwiftUI
-
+//プロフィール画面
 struct ProfileHost: View {
     
+    @Environment(\.editMode) var mode // 編集モードのビュー属性を追加
     @State var draftProfile = Profile.default
     
     var body: some View {
     
         VStack(alignment: .leading, spacing: 20) {
+            HStack {
+                Spacer()
+                EditButton() // 編集ボタン
+            }
             ProfileSummary(profile: draftProfile)
         }
         .padding()
